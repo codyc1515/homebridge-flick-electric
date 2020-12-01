93146d0365b71b2a9f004609da1bb632f9f3f032
codyc1515 <50791984+codyc1515@users.noreply.github.com>
2020-12-01 21:48:07 +1300

    Major changes
    * Improve reliability
    * Migrate from Temperature Sensor to Light Sensor

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var Service;
-var Characteristic;
+var Service,
+	Characteristic;
 
 var request = require("request");
 
@@ -15,11 +15,10 @@ function FlickElectric(log, config) {
     this.log = log;
     this.name = config["name"] || "Flick Electric";
     this.token = config["token"];
-    this.email = config["email"];
-    this.password = config["password"];
     this.debug = config["debug"] || false;
 
-    this.CurrentPrice = null;
+	this.import = 0;
+	this.export = 0;
 
     // Start running the refresh process
     try {
@@ -27,7 +26,9 @@ function FlickElectric(log, config) {
         this._refresh();
 
         // Refresh periodically
-        setInterval(function() {this._refresh();}.bind(this), 600000);
+        setInterval(function() {
+			this._refresh();
+		}.bind(this), 600000);
     }
     catch(err) {this.log("An unknown error occured", err);}
 }
@@ -37,6 +38,42 @@ FlickElectric.prototype = {
     identify: function(callback) {
         this.log("identify");
         callback();
+    },
+
+    getServices: function() {
+		// Light Sensor service - Import
+		this.LightSensorImport = new Service.LightSensor(this.name + " Import", "Import");
+		this.LightSensorImport
+			.getCharacteristic(Characteristic.CurrentAmbientLightLevel)
+			.setProps({
+				minValue: 0,
+				maxValue: 10000,
+				minStep: 0.01
+			});
+
+		// Light Sensor service - Export
+		this.LightSensorExport = new Service.LightSensor(this.name + " Export", "Export");
+		this.LightSensorExport
+			.getCharacteristic(Characteristic.CurrentAmbientLightLevel)
+			.setProps({
+				minValue: 0,
+				maxValue: 10000,
+				minStep: 0.01
+			});
+
+		// Accessory Information service
+        this.AccessoryInformation = new Service.AccessoryInformation();
+        this.AccessoryInformation
+            .setCharacteristic(Characteristic.Name, this.name)
+            .setCharacteristic(Characteristic.Manufacturer, "Flick Electric")
+            .setCharacteristic(Characteristic.Model, "Freestyle")
+            .setCharacteristic(Characteristic.FirmwareRevision, "1.0.0");
+
+        return [
+			this.AccessoryInformation,
+			this.LightSensorImport,
+			this.LightSensorExport
+        ];
     },
 
     _refresh: function() {
@@ -53,12 +90,21 @@ FlickElectric.prototype = {
                     body = JSON.parse(body);
 
                     if(body.kind === "mobile_provider_price") {
-                        // Find & set the CurrentTemperature
-                        this.CurrentTemperature = body.needle.price;
+						// Get the Import price and add GST
+						this.import = body.needle.price * 1.15;
+                        if(this.debug) {this.log("Import price", this.import);}
 
-                        if(this.debug) {console.log('Obtained pricing successfully', body, this.CurrentTemperature);}
+                        this.LightSensorImport.getCharacteristic(Characteristic.CurrentAmbientLightLevel).updateValue(this.import);
 
-                        this.TemperatureSensor.getCharacteristic(Characteristic.CurrentTemperature).updateValue(this.CurrentTemperature);
+						// Get the Export price
+						for (var i = 0, len = body.needle.components.length; i < len; i++) {
+							if(body.needle.components[i].charge_setter == 'generation') {
+								this.export = body.needle.components[i].value;
+								if(this.debug) {this.log("Export price", this.export);}
+
+								this.LightSensorExport.getCharacteristic(Characteristic.CurrentAmbientLightLevel).updateValue(this.export);
+							}
+						}
                     }
                     else {this.log("Could not process pricing response");}
                 }
@@ -68,45 +114,9 @@ FlickElectric.prototype = {
         }.bind(this));
     },
 
-    getServices: function() {
-        this.TemperatureSensor = new Service.TemperatureSensor(this.name);
-        this.TemperatureSensor
-            .getCharacteristic(Characteristic.CurrentTemperature)
-            .setProps({
-				minValue: 0,
-				maxValue: 1000,
-				minStep: 0.001
-			})
-            .on('get', this._getValue.bind(this, "CurrentTemperature"));
-
-        this.informationService = new Service.AccessoryInformation();
-        this.informationService
-            .setCharacteristic(Characteristic.Name, this.name)
-            .setCharacteristic(Characteristic.Manufacturer, "Flick Electric")
-            .setCharacteristic(Characteristic.Model, "Freestyle")
-            .setCharacteristic(Characteristic.FirmwareRevision, "1.0.0");
-
-        return [
-            this.TemperatureSensor,
-            this.informationService
-        ];
-    },
-
     _getValue: function(CharacteristicName, callback) {
         if(this.debug) {this.log("GET", CharacteristicName);}
-
-        switch (CharacteristicName) {
-
-            case "CurrentTemperature":
-                this._refresh();
-                callback(null, this.CurrentTemperature);
-            break;
-
-            default:
-                this.log("Unknown CharacteristicName called", CharacteristicName);
-                callback();
-            break;
-        }
+		callback(null);
     }
 
 };
